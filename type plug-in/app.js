57a61edef18db01d69a1884084cae8ba16b26efc
hasnainzaidi2020 <66158742+hasnainzaidi2020@users.noreply.github.com>
2021-09-26 21:54:59 +0500

    Add files via upload

---
+++ type plug-in/app.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    // Waits 1000ms after typing "First"
+    strings: ['Web Developer','Full Stack Web Developer','Front End Web Developer','.Net Developer','PHP Developer' ,'YouTuber', 'Blogger', 'Gamer'],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true,
+});  
