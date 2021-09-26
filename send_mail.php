e82ffc506fae56b38cd8c2d12b6aff4b7afde745
hasnainzaidi2020 <66158742+hasnainzaidi2020@users.noreply.github.com>
2021-09-27 01:16:35 +0500

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,24 @@
+<?php
+ if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = "Name " . $name .' Message  '. $_POST['message'];
+    $to = "hzaidi852@gmail.com";
+    $header = "From :".$email;
+
+    if( mail($to,$subject,$message,$header))
+    {
+      header('Location: response.html');
+    }
+    else{
+      echo "<script>
+      alert('Server Error');
+      window.location.assign('index.php');
+      </script>
+      
+      ";
+    }
+
+ }
+?>
